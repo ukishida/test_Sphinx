7502d95a35716458969096e6e2181cbb26dc59cf
ukishida <kishida.yuki.36s@st.kyoto-u.ac.jp>
2023-07-22 12:49:30 +0900

    update docs

--- Sphinx_files/conf.py
+++ Sphinx_files/conf.py
@@ -24,5 +24,9 @@ language = 'ja'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+#html_theme = 'alabaster'
+#html_static_path = ['_static']
+
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
